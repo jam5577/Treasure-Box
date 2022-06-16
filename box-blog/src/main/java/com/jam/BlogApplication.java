ef474e0bd5bf4d6d6b1715ec58b6d879cffa67a1
jam5577 <806420570@qq.com>
2022-06-16 22:28:37 +0800

    #init

---
+++ box-blog/src/main/java/com/jam/BlogApplication.java
@@ -0,0 +1,11 @@
+package com.jam;
+
+/**
+ * @program: treasure-box
+ * @description: 主启动类
+ * @author: Mr.Pu
+ * @create: 2022-06-16 17:26
+ **/
+
+public class BlogApplication {
+}
---
+++ box-gateway/src/main/java/com/jam/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.jam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: treasure-box
+ * @description: 网关启动类
+ * @author: Mr.Pu
+ * @create: 2022-06-16 19:49
+ **/
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+}
